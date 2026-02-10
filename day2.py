7c03e12ceedd093312de6f788e998881da22258c
NAGA ADITHYA <nagaadithyavuppalapati@gmail.com>
2026-02-09 21:23:19 -0800

    Day 2

--- day2.py
+++ day2.py
@@ -1,13 +1,13 @@
-print(type(123))
-print(type(123.45))
-print(type("Hello"))
-print(type(True))
+# print(type(123))
+# print(type(123.45))
+# print(type("Hello"))
+# print(type(True))
 
-#BMI claculator
-weight = float(input("Enter your weight in kg: "))
-height = float(input("Enter your height in m: "))
-bmi = weight / (height * height)
-print("Your BMI is: ", bmi)
+# #BMI claculator
+# weight = float(input("Enter your weight in kg: "))
+# height = float(input("Enter your height in m: "))
+# bmi = weight / (height * height)
+# print("Your BMI is: ", bmi)
 
 print("Welcome to the Tip Calculator.")
 total_bill = float(input("What was the total bill? "))
@@ -16,5 +16,5 @@ tip_percentage = int(input("How much tip would you like to give? 10, 12 or 15? "
 # total_amount = total_bill + tip_amount
 total_amount = total_bill * (1 + (tip_percentage / 100))
 people = int(input("How many people to split the bill? "))
-print("Each person should pay: ", total_amount / people)
+print("Each person should pay: ", round(total_amount / people, 2))
 
